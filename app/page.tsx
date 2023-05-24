c991c0d5e743368bc7b98d654a63c80b6d49d04d
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-05-25 00:45:54 +0200

    Force dynamic streaming

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 const AsyncComponentTest = async () => {
 	await new Promise(resolve => {
 		setTimeout(resolve, 3_000);
