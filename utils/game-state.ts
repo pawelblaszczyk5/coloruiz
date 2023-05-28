44c05cecf60074ec2ccf6862521f37e3e516b1d3
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-05-28 20:11:57 +0200

    Init game state utilities

---
+++ utils/game-state.ts
@@ -0,0 +1,55 @@
+import { cookies } from 'next/headers';
+import { z } from 'zod';
+import { env } from '~/app/env.mjs';
+import { signCookie, unsignCookie } from '~/utils/signed-cookie';
+
+const COOKIE_NAME = 'coloruiz-game-state';
+
+const colorValueSchema = z.number().min(0).max(255);
+
+const gameStateSchema = z.object({
+	level: z.number().min(1),
+	score: z.number().min(0),
+	currentColor: z.tuple([colorValueSchema, colorValueSchema, colorValueSchema]),
+});
+
+type GameState = z.infer<typeof gameStateSchema>;
+
+const generateRandomColor = () => {
+	return [0, 0, 0] satisfies GameState['currentColor'];
+};
+
+export const getDefaultGameState = () => {
+	return {
+		level: 1,
+		score: 0,
+		currentColor: generateRandomColor(),
+	} satisfies GameState;
+};
+
+export const getGameState = async () => {
+	const cookieValue = cookies().get(COOKIE_NAME)?.value;
+
+	if (!cookieValue) return null;
+
+	const unsignedCookieValue = await unsignCookie(cookieValue, env.GAME_STATE_COOKIE_SECRET);
+
+	if (!unsignedCookieValue) return null;
+
+	try {
+		return gameStateSchema.parse(JSON.parse(unsignedCookieValue));
+	} catch {
+		return null;
+	}
+};
+
+export const saveGameState = async (state: GameState) => {
+	const signedCookieValue = await signCookie(JSON.stringify(state), env.GAME_STATE_COOKIE_SECRET);
+
+	cookies().set(COOKIE_NAME, signedCookieValue, {
+		secure: true,
+		sameSite: true,
+		httpOnly: true,
+		maxAge: 60 * 60 * 24 * 365, // full year,
+	});
+};
