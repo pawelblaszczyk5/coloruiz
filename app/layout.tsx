69b151cbbd967a5e50311f0a9fb3266e47de8b44
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-05-31 17:04:50 +0200

    Layout adjustment

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,9 +26,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
 			<body className="flex flex-col gap-8 bg-stone-100 p-8 text-stone-950 dark:bg-stone-800 dark:text-stone-200">
 				<Providers>
 					<Header />
-					<main className="mx-auto flex max-w-prose flex-col items-center gap-8 py-12 md:gap-12 md:py-20">
-						{children}
-					</main>
+					<main className="mx-auto flex max-w-prose flex-col items-center gap-10 py-12 md:py-20">{children}</main>
 				</Providers>
 			</body>
 		</html>
