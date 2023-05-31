f6e34eb0442514ea03d4c7ac070eb26cc6c045df
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-05-31 18:46:09 +0200

    Hex input

--- app/game/_components/Playboard.tsx
+++ app/game/_components/Playboard.tsx
@@ -16,8 +16,10 @@ import { cn } from '~/utils/classnames';
 import { type GameState } from '~/utils/game-state';
 import LucideChevronDown from '~icons/lucide/chevron-down.jsx';
 import LucideChevronUp from '~icons/lucide/chevron-up.jsx';
+import LucideHash from '~icons/lucide/hash.jsx';
 
 const DIGIT_REGEX = /\d/u;
+const HEX_REGEX = /[\da-f]*/iu;
 
 const SingleValueInput = ({
 	children,
@@ -25,7 +27,7 @@ const SingleValueInput = ({
 	...props
 }: { children: ReactNode; error?: string | undefined } & Pick<
 	ComponentProps<typeof NumberInput>,
-	'value' | 'onBlur' | 'onChange' | 'name'
+	'value' | 'onChange' | 'name'
 >) => {
 	const hasError = typeof error === 'string';
 	const errorId = useId();
@@ -80,10 +82,53 @@ const SingleValueInput = ({
 	);
 };
 
+const HexInput = ({
+	error,
+	children,
+	...props
+}: { error?: string | undefined; children: ReactNode } & Pick<
+	ComponentProps<'input'>,
+	'value' | 'onChange' | 'name'
+>) => {
+	const hasError = typeof error === 'string';
+	const id = useId();
+
+	const inputId = `${id}-input`;
+	const errorId = `${id}-error`;
+
+	return (
+		<div className="flex flex-col gap-2">
+			<label className="text-lg" htmlFor={inputId}>
+				{children}
+			</label>
+			<div className="relative">
+				<div className="pointer-events-none absolute left-0 top-0 grid aspect-square h-11 w-11 place-items-center text-lg text-stone-500">
+					<LucideHash />
+				</div>
+				<input
+					className={cn(
+						'w-full rounded-md border-2 bg-stone-50 py-2 pl-10 pr-4 text-base outline-2 outline-offset-2 outline-fuchsia-500 focus-visible:outline dark:bg-stone-900',
+						hasError ? 'border-rose-700' : 'border-teal-700',
+					)}
+					id={inputId}
+					aria-describedby={hasError ? errorId : undefined}
+					aria-errormessage={hasError ? errorId : undefined}
+					autoComplete="off"
+					spellCheck={false}
+					maxLength={6}
+					type="text"
+					{...props}
+				/>
+			</div>
+			{hasError && <p className="text-rose-700 dark:text-rose-400">{error}</p>}
+		</div>
+	);
+};
+
 export const Playboard = ({ state, proceedGame }: { state: GameState; proceedGame: typeof proceedGameAction }) => {
 	const [r, g, b] = state.currentColor;
 
-	const [inputMode, setInputMode] = useState<'hex' | 'separate'>('separate');
+	const [inputMode, setInputMode] = useState<'hex' | 'separate'>('hex');
 
 	return (
 		<div className="flex flex-col items-center gap-10">
@@ -98,12 +143,12 @@ export const Playboard = ({ state, proceedGame }: { state: GameState; proceedGam
 				{match(inputMode)
 					.with('separate', () => (
 						<>
-							<SingleValueInput name="color-r">Red</SingleValueInput>
-							<SingleValueInput name="color-g">Green</SingleValueInput>
-							<SingleValueInput name="color-b">Blue</SingleValueInput>
+							<SingleValueInput name="color-r">Red value</SingleValueInput>
+							<SingleValueInput name="color-g">Green value</SingleValueInput>
+							<SingleValueInput name="color-b">Blue value</SingleValueInput>
 						</>
 					))
-					.with('hex', () => <></>)
+					.with('hex', () => <HexInput>Hex value</HexInput>)
 					.exhaustive()}
 				<Button>Submit answer</Button>
 			</form>
