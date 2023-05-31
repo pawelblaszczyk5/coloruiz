4377071db56643546abf63164ffb76c9443edc7a
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2023-05-31 17:16:40 +0200

    Introduction copy

--- app/game/_components/Introduction.tsx
+++ app/game/_components/Introduction.tsx
@@ -4,9 +4,22 @@ import { Button } from '~/components/button';
 export const Introduction = () => {
 	return (
 		<>
-			<h1>Introduction</h1>
+			<h1 className="text-3xl">How to play?</h1>
+			<div className="flex flex-col gap-3">
+				<p className="md:text-lg">
+					In each level you&apos;l get a random color displayed. Your goal is to assign RGB (red, green, blue) values to
+					a given color.
+				</p>
+				<p className="md:text-lg">
+					Every level gets progressively harder and you need to get closer to actual values to proceed. However, the
+					score available to get also increase.
+				</p>
+				<p className="md:text-lg">
+					At all time you can also switch between providing separate RGB values or the hexadecimal value of the color.
+				</p>
+			</div>
 			<form action={startGame}>
-				<Button>Start game</Button>
+				<Button>Start a new game</Button>
 			</form>
 		</>
 	);
